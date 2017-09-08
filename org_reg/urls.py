d8f9648c4ce14169ed92b548b3f997442ca5c887
Tom Rosenbloom <TomRosenbloom@users.noreply.github.com>
2017-09-08 12:43:18 +0100

    cbv (FormView) for organisation profile

--- org_reg/urls.py
+++ org_reg/urls.py
@@ -6,6 +6,7 @@ app_name = 'org_reg'
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^signup/$',views.signup,name='signup'),
-    url(r'^profile/$',views.profile,name='profile'),
+    url(r'^profile/$',views.ProfileView.as_view(),name='profile'),
     url(r'^login/$',views.org_login,name='login'),
+    url(r'^foo/$',views.Foo.as_view())
 ]
--- org_reg/views.py
+++ org_reg/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView, FormView
 from . import forms
 from .forms import OrganisationForm
 from backend.models import OrganisationType
 
 # Create your views here.
 
+class Foo(TemplateView):
+    template_name = 'org_reg/foo.html'
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['foo'] = 'foo'
+        return context
+
 def org_login(request):
     return render(request,'org_reg/org_login.html')
 
-def profile(request):
-    form = OrganisationForm()
-    return render(request, 'org_reg/profile.html', {'form': form, 'types': OrganisationType.objects.all()})
+class ProfileView(FormView):
+    form_class = OrganisationForm
+    template_name = 'org_reg/profile.html'
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['types'] = OrganisationType.objects.all()
+        return context
+
+# def profile(request):
+#     form = OrganisationForm()
+#     return render(request, 'org_reg/profile.html', {'form': form, 'types': OrganisationType.objects.all()})
 
 def signup(request):
     return render(request,'org_reg/signup.html')
