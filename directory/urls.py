ceee4f1cd010ad12f7a4da614cad604574ab402d
Tom Rosenbloom <TomRosenbloom@users.noreply.github.com>
2017-09-21 10:21:37 +0100

    new url for search results detail view

--- directory/urls.py
+++ directory/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^$',views.IndexView.as_view(),name='index'),
     url(r'^list/$',views.OrganisationListView.as_view(),name='list'),
     url(r'^list/(?P<pk>[-\w+])$',views.OrganisationDetailView.as_view()),
+    url(r'^detail/(?P<pk>[-\w+])$',views.OrganisationDetailView.as_view(),name='detail'),
     #url(r'^search/$',views.search, name='search'),
     url(r'^export/csv/$', views.export_organisations_csv, name='export_organisations_csv'),
     url(r'^filter/$',views.OrganisationFilterView.as_view(), name='filter')
