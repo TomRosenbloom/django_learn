79b4efd6e4a0e898d3e28997b3333c0a4157e736
Tom Rosenbloom <TomRosenbloom@users.noreply.github.com>
2017-10-20 16:56:00 +0100

    corrected url pattern

--- directory/urls.py
+++ directory/urls.py
@@ -10,8 +10,8 @@ app_name = 'directory'
 urlpatterns = [
     url(r'^$',views.IndexView.as_view(),name='index'),
     url(r'^list/$',views.OrganisationListView.as_view(),name='list'),
-    url(r'^list/(?P<pk>[-\w+])$',views.OrganisationDetailView.as_view()),
-    url(r'^detail/(?P<pk>[-\w+])$',views.OrganisationDetailView.as_view(),name='detail'),
+    url(r'^list/(?P<pk>\d+)$',views.OrganisationDetailView.as_view()),
+    url(r'^detail/(?P<pk>\d+)$',views.OrganisationDetailView.as_view(),name='detail'),
     #url(r'^search/$',views.search, name='search'),
     url(r'^export/csv/$', views.export_organisations_csv, name='export_organisations_csv'),
     url(r'^filter/$',views.OrganisationFilterView.as_view(), name='filter')
