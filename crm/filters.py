c1434e777b6adb17fbd1b66f05c83edfb6494535
Tom Rosenbloom <TomRosenbloom@users.noreply.github.com>
2017-11-05 12:19:12 +0000

    look-up types in filter form; contains, starts with

--- crm/filters.py
+++ crm/filters.py
@@ -1,12 +1,23 @@
 from django import forms
 from backend.models import Organisation, OrganisationType
 import django_filters
+from django_filters import filters
+
+filters.LOOKUP_TYPES = [
+    ('', '---------'),
+    ('istartswith', 'Starts with'),
+    ('icontains', 'Contains'),
+]
+# https://docs.djangoproject.com/en/1.11/ref/models/querysets/#field-lookups
+# ...lots of useful options: range, date, year, month
+# ...but 'search' deprecated since 1.10
 
 class OrganisationFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='icontains')
+    name = django_filters.CharFilter(lookup_expr=['icontains','istartswith'])
+    telephone = django_filters.CharFilter(lookup_expr=['icontains','istartswith'])
     types = django_filters.ModelChoiceFilter(queryset=OrganisationType.objects.all(), empty_label='Organisation type')
 
     class Meta:
         model = Organisation
         #fields = ['name', 'aims_and_activities', 'postcode', 'address', 'types']
-        fields = ['name', 'types','postcode']
+        fields = ['name', 'types', 'telephone', 'postcode']
